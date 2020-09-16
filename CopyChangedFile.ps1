9b6a98c346f013dac34e24962e141609b00edc5e
Funkiezz <nareshkabra@aol.in>
2020-09-16 12:06:04 +1000

    Added PS1 file to repo

---
+++ CopyChangedFile.ps1
@@ -0,0 +1,13 @@
+﻿Write-Host "Running change file PS script"
+$Home_dir = pwd | Select -exp Path
+Write-Host $Home_dir
+
+$changedFiles = Get-Content -Path ${Home_dir}"/changedFiles.txt" | Where-Object { $_ -like "*MAPD*.tra:CHANGED*" }
+Write-Host $changedFiles
+$startString = $changedFiles.IndexOf("MAPD/")
+$startString = $startString + 5
+$endString = $changedFiles.IndexOf(".tra")
+$endStringlen = $endString - $startString
+$filename = $changedFiles.Substring($startString,$endStringlen)
+Write-Host $filename
+##[System.Environment]::Exit(1)
